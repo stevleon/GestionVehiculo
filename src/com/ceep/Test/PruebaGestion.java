fdf81f73872610884c8917fa5be065db3621daf0
stevleon <stevleon154@gmail.com>
2021-09-28 12:25:32 +0200

    Initial commit

---
+++ src/com/ceep/Test/PruebaGestion.java
@@ -0,0 +1,16 @@
+
+package com.ceep.Test;
+
+import com.ceep.domain.Vehiculo;
+
+
+public class PruebaGestion 
+{
+    public static void main(String[] args)
+    {
+        Vehiculo v1= new Vehiculo("3524GFD","FORD","FOCUS","ROJO",320,true);
+        System.out.println(v1);
+                
+    }
+  
+}
